cf428c599e36ea7b926f399d26196cce2df16c1e
Shantanu-Meta <Shantanubhs1985@gmail.com>
2024-01-14 23:21:54 +0530

    Added Search Feature in app

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,33 +17,34 @@ export default class Navbar extends Component {
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
                 </svg>
             </button>
+
             <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li>
-                <Link to="/" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white  hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 focus:text-blue-700 focused">General</Link>
-                </li>
-                <li>
-                <Link to="/science" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white  hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 focus:text-blue-700 focused">Science</Link>
-                </li>
-                <li>
-                <Link to="/sports" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white  hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 focus:text-blue-700 focused">Sports</Link>
-                </li>
-                <li>
-                <Link to="/technology" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent focus:text-blue-700">Technology</Link>   
-                </li>
-                <li>
-                <Link to="/health" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent focus:text-blue-700">Health</Link>
-                </li>
-                <li>
-                <Link to="/business" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent focus:text-blue-700">Business</Link>
-                </li>
-                <li>
-                <Link to="/entertainment" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent focus:text-blue-700">Entertainment</Link>
-                </li>
-                <li>
-                <Link to="/About" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent focus:text-blue-700">About</Link>
-                </li>
-            </ul>
+              <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                  <li>
+                  <Link to="/" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white  hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 focus:text-blue-700 focused">General</Link>
+                  </li>
+                  <li>
+                  <Link to="/science" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white  hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 focus:text-blue-700 focused">Science</Link>
+                  </li>
+                  <li>
+                  <Link to="/sports" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white  hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 focus:text-blue-700 focused">Sports</Link>
+                  </li>
+                  <li>
+                  <Link to="/technology" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent focus:text-blue-700">Technology</Link>   
+                  </li>
+                  <li>
+                  <Link to="/health" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent focus:text-blue-700">Health</Link>
+                  </li>
+                  <li>
+                  <Link to="/business" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent focus:text-blue-700">Business</Link>
+                  </li>
+                  <li>
+                  <Link to="/entertainment" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent focus:text-blue-700">Entertainment</Link>
+                  </li>
+                  <li>
+                  <Link to="/About" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent focus:text-blue-700">About</Link>
+                  </li>
+              </ul>
             </div>
         </div>
         </nav>
--- src/components/Newsarea.js
+++ src/components/Newsarea.js
@@ -17,12 +17,14 @@ export default class Newsarea extends Component {
       totalResults: null, 
       load: true,
       progress: 0, 
+      searchedData: "",
     }
     document.title = `NewsMonk | ${this.capitalize(this.props.category)}`; 
   }
+  
 
    fetchData = async ()=>{
-    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=${process.env.API_KEY}&page=${this.state.page}&pageSize=5`
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=${process.env.REACT_APP_API_KEY}&page=${this.state.page}&pageSize=10`
     let response =await fetch(url);
     let data =await response.json(); 
     this.setState({
@@ -32,39 +34,73 @@ export default class Newsarea extends Component {
     });   
   }
 
-  async componentDidMount(){
+  searchData= (value)=>{
+    let newArticles = []; 
+    this.state.articles.forEach(element => {
+      if(element.title.includes(value) || element.description.includes(value)) newArticles.push(element); 
+    });
+    this.setState({articles: newArticles}); 
+  }
+
+  async componentDidMount(){    window.scrollTo(0,0); 
     this.setState({progress: 10})
     await this.fetchData();    
     this.setState({progress: 100})
   }
 
-  fetchMoreData = async ()=>{
+  fetchMoreData = async ()=>{    console.log("fetch More")
     this.setState({page: this.state.page + 1}, ()=>{
       this.fetchData(); 
     })
-     
+  }
+
+  searchArticle = ()=>{
+    if(this.state.searchedData==="") return; 
+    let newArticles = []; 
+    for(let i=0; i<this.state.articles.length; i++){
+      let ele = this.state.articles[i]; 
+      if(ele.title && ele.description && (ele.title.includes(this.state.searchedData) || ele.description.includes(this.state.searchedData))){
+        newArticles.push(ele); 
+      }
+    }
+    if(newArticles.length !==0 )
+      this.setState({articles: newArticles}); 
+    else alert("No Match found")
   }
 
   capitalize(string){
     return string.charAt(0).toUpperCase() + string.slice(1); 
   }
 
-  render(){
+  render(){   
     return(
       <div className='w-full'>
         <LoadingBar
           color='#f11946'
           progress={this.state.progress}
         />
-        <div className='w-[90%] mx-auto mt-[5rem]'>
-        {this.state.load && <Loading/>}
+        <div className='w-[90%] mx-auto mt-[5rem] pb-[0.5rem]'>
         {this.state.articles.length>0 && <h1 className='text-3xl text-center mb-3'>NewsMonk - {this.capitalize(this.props.category)}</h1>}
+        { this.state.articles.length>0 && <div className='w-[30%] mx-auto my-[1rem]'>
+              <form autoComplete="off">   
+                    <div className="relative">
+                        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                            </svg>
+                        </div>
+                        <input onChange={(e) => {
+                          this.setState({searchedData: e.target.value})}} type="text" id="default-search" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search news.." required/>
 
+                          <button type="button" className="text-white absolute end-1 top-[50%] translate-y-[-50%]  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-1 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={this.searchArticle}>Search</button>
+                    </div>
+              </form>
+        </div>}
           <InfiniteScroll
             dataLength={this.state.articles.length}
-            next={this.fetchMoreData}
-            hasMore={true}
-            loader={!(this.state.totalResults===this.state.articles.length) && this.state.articles.length>0 && <Loading/>}
+            next={this.state.searchedData.length===0 && this.fetchMoreData}
+            hasMore={this.state.totalResults!==this.state.articles.length}
+            loader={this.state.searchedData.length===0 && <Loading/>}
           >
           <div className='flex align-center justify-evenly flex-wrap gap-1'>
             {this.state.articles.map((ele) =>{
